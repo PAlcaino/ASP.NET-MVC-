2b5e662d0ce323e04ccb4b9221ff4bf5c50eff49
Pablo <Pablo@DESKTOP-MAPMFIN>
2018-11-18 23:34:11 -0300

    Prueba de concepto con boostrap, ASP.net MVC 5, Razor

---
+++ GeneracionInvitaciones/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using GeneracionInvitaciones.Helpers;
+using GeneracionInvitaciones.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneracionInvitaciones.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            DateTime fecha = DateTime.Now;
+            string fechareunion = fecha.AddDays(5).ToShortDateString();          
+
+            ViewBag.saludoInicial = "PROXIMA REUNION: " + fechareunion;
+            return View();
+        }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ViewResult> RsvpForm(RespuestaInvitado respuesta)
+        {
+            string to = "palcaino.lleite@gmail.com";
+            string subject = "Notificacion de Reunion";
+            string body = string.Empty;
+
+            if(ModelState.IsValid)
+            {
+                body = string.Format("Gracias"+ ViewBag.Nombre);
+                await MailHelper.SendEmail(to,subject,body);
+                return View("Gracias", respuesta);
+            }
+            return View();
+        }
+    }
+}
---
+++ GeneracionInvitaciones/Models/RespuestaInvitado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneracionInvitaciones.Models
+{
+    public class RespuestaInvitado
+    {
+        [Required(ErrorMessage = "Debe ingresar un nombre")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Debe ingresar un correo")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Ingrese un email valido.")]
+        public string Correo { get; set; }
+        [Required(ErrorMessage = "Debe ingresar un telefono")]
+        public string Telefono { get; set; }
+        [Required(ErrorMessage = "Debe seleccionar una opcion")]
+        public bool? Asistira { get; set; }
+    }
+}
